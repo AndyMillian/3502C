869ebb5602a002be3bacb19583470ad6ff4a8094
AndyMillian <49721818+AndyMillian@users.noreply.github.com>
2022-03-14 07:22:02 -0400

    Update GitHubRepoFile.java

--- GitHubRepoFile.java
+++ GitHubRepoFile.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class GitHubRepoFile {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner s = new Scanner(System.in);
+        int res;
+        int inp = 0;
+        System.out.println("What is the first Number?");
+        int one = s.nextInt();
+        System.out.println("What is the second Number?");
+        int two = s.nextInt();
+        System.out.println("Which would you like to do?\n1. Add\n2. Subtract");
+        inp = s.nextInt();
+        if (inp == 1){
+            res = one+two;
+            System.out.println("The Result is: "+res);
+        }
+
     }
 }
